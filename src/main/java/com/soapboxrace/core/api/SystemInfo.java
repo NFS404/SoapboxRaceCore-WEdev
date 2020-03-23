dd3ec8a366b78fc051671ed5b539955e081eb706
VladManyanov <mvv0105nfs@gmail.com>
2020-03-23 13:07:25 +0700

    8202 TE abort code

--- src/main/java/com/soapboxrace/core/api/SystemInfo.java
+++ src/main/java/com/soapboxrace/core/api/SystemInfo.java
@@ -39,7 +39,7 @@ public class SystemInfo {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		systemInfo.setVersion("sbrw_wev2_07032020f4");
+		systemInfo.setVersion("sbrw_wev2_07032020f5");
 		return systemInfo;
 	}
 }
--- src/main/java/com/soapboxrace/core/bo/LegitRaceBO.java
+++ src/main/java/com/soapboxrace/core/bo/LegitRaceBO.java
@@ -59,8 +59,8 @@ public class LegitRaceBO {
 			isSingleText = "SP";
 		}
 		
-		// 0 - quitted from race, 22 - finished, 518 - escaped from SP pursuit, 266 - busted on SP & MP pursuit
-		if (arbitrationPacket.getFinishReason() != 0 && arbitrationPacket.getFinishReason() != 266) {
+		// 0 - quitted from race, 22 - finished, 518 - escaped from SP pursuit, 266 - busted on SP & MP pursuit, 8202 - aborted on TE
+		if (arbitrationPacket.getFinishReason() != 0 && arbitrationPacket.getFinishReason() != 266 && arbitrationPacket.getFinishReason() != 8202) {
 			finishReasonLegit = false;
 		}
 		if (!legit && !finishReasonLegit) {
