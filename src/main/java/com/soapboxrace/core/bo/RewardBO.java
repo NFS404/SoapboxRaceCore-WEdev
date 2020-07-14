855b5a9d04d91267babfeac32a833ed2283478dd
VladManyanov <mvv0105nfs@gmail.com>
2020-07-14 15:54:32 +0700

    disable boost rewards for now #2

--- src/main/java/com/soapboxrace/core/bo/RewardBO.java
+++ src/main/java/com/soapboxrace/core/bo/RewardBO.java
@@ -98,18 +98,18 @@ public class RewardBO {
 		if (productEntity.getStars() != null && productEntity.getStars() == 4 && productEntity.getProductType().contentEquals("SKILLMODPART")) {
 			achievementsBO.applyDropAchievements(personaEntity, AchievementType.WEV2_EARNSKILL);
 		}
-		if (isTeamRace) {
-			System.out.println("### TEAMS: Player " + personaEntity.getName() + " has got the item: " + productEntity.getProductTitle());
-		}
+//		if (isTeamRace) {
+//			System.out.println("### TEAMS: Player " + personaEntity.getName() + " has got the item: " + productEntity.getProductTitle());
+//		}
 		LuckyDrawItem luckyDrawItem = dropBO.copyProduct2LuckyDraw(productEntity);
 		boolean inventoryFull = inventoryBO.isInventoryFull(productEntity, personaEntity);
-        if (productEntity.getProductTitle().contains("SPEEDBOOST")) {
-        	int sbAmount = parameterBO.getIntParam("REWARD_SB_AMOUNT");
-        	System.out.println("### Player " + personaEntity.getName() + " has recieved a " + sbAmount + " SpeedBoost reward.");
-			UserEntity userEntity = personaEntity.getUser();
-			userEntity.setBoost(userEntity.getBoost() + sbAmount);
-			userDao.update(userEntity);
-		}
+//      if (productEntity.getProductTitle().contains("SPEEDBOOST")) {
+//        	int sbAmount = parameterBO.getIntParam("REWARD_SB_AMOUNT");
+//        	System.out.println("### Player " + personaEntity.getName() + " has recieved a " + sbAmount + " SpeedBoost reward.");
+//			UserEntity userEntity = personaEntity.getUser();
+//			userEntity.setBoost(userEntity.getBoost() + sbAmount);
+//			userDao.update(userEntity);
+//		}
 		if (inventoryFull) {
 			luckyDrawItem.setWasSold(true);
 			if (parameterBO.getBoolParam("ENABLE_ECONOMY")) {
