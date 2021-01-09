745880e4b5bf4129f4847784482f2bd6c5a10675
VladManyanov <mvv0105nfs@gmail.com>
2021-01-09 14:25:46 +0700

    typo error

--- src/main/java/com/soapboxrace/core/bo/DiscordBO.java
+++ src/main/java/com/soapboxrace/core/bo/DiscordBO.java
@@ -22,13 +22,19 @@ public class DiscordBO {
 		switch (errorCode) {
 		case 100:
 			errorStr = "???";
+			break;
+		case 101:
+			errorStr = "???";
+			break;
 		case 105:
 			errorStr = "PEER_TIMEOUT";
+			break;
 		case 106:
 			errorStr = "HANDSHAKE_ERROR";
+			break;
 		}
 		String message = ":heavy_minus_sign:"
-        		+ "\n:candle: **| " + playerName + "** вылетел с онлайн-заезда / was kicked from online-event, *error " + errorCode + " (" + errorStr + ").";
+        		+ "\n:candle: **| " + playerName + "** вылетел с онлайн-заезда / was kicked from online-event, *error " + errorCode + " (" + errorStr + ")*.";
 		discordBot.sendMessage(message);
 	}
 }
